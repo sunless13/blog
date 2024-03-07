5fd74189e9a30ee7e9d4593c9fcef4ce0b030e9c
sunless <sunlesskelv@gmail.com>
2024-03-07 19:54:54 +0300

    pressing 't' toggles the toc

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,24 @@
 const tocTitle = document.querySelectorAll("a")[1];
 const toc = document.querySelectorAll("ul")[0];
 
-// adding click event listener to show the topics
+// adding click event listener to name "Table of Contents"
 tocTitle.addEventListener("click", () => {
+  // makes the content of the toc visible
   toc.classList.toggle("show");
+});
+
+// Keyboard shortcuts
+// i'm not naming that variable "event" .... i'll come up with another name later
+document.addEventListener("keypress", (varName) => {
+  switch (varName.key) {
+    // table of contents toggle
+    case "t":
+      toc.classList.toggle("show");
+      break;
+
+    default:
+      //console.log("Key not assigned");
+      alert("Pussy!");
+      break;
+  }
 });
