ce90db12e15477d68475102a28a47726ba0e9620
sunless <sunlesskelv@gmail.com>
2024-03-07 15:15:17 +0300

    moved toc from aside to the navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,8 @@
-// assigning the topics to a constant variable
-const topic = document.querySelectorAll("h2");
-const subTopic = document.querySelectorAll("ol");
+// assigning the elements to variables
+const tocTitle = document.querySelectorAll("a")[1];
+const toc = document.querySelectorAll("ul")[0];
 
-// adding event listener to every topic clicked
-topic[0].addEventListener("click", () => {
-  subTopic[0].classList.toggle("active");
-  subTopic[1].classList.toggle("active");
-  subTopic[2].classList.toggle("active");
-});
-topic[1].addEventListener("click", () => {
-  subTopic[0].classList.toggle("active");
-});
-topic[2].addEventListener("click", () => {
-  subTopic[1].classList.toggle("active");
-});
-topic[3].addEventListener("click", () => {
-  subTopic[2].classList.toggle("active");
+// adding click event listener to show the topics
+tocTitle.addEventListener("click", () => {
+  toc.classList.toggle("show");
 });
